e4a42be9f84af72765396b20748e2235838f78af
Temick <arty.garin@yandex.ru>
2023-11-13 23:29:10 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -2,29 +2,44 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import { DoTask } from './components/DoTask';
 import { Task } from './components/Task';
+import { v4 } from 'uuid';
+import { getTasks, removeTask, setTask, updateTask } from './store/store';
 
 
 function App() {
   const [tasks, setTasks] = useState([])
 
   useEffect(() => {
-      const storedTasks = localStorage.getItem('tasks');
+      const storedTasks = getTasks()
       if (storedTasks) {
-          setTasks(JSON.parse(storedTasks));
+          setTasks(storedTasks);
       }
   }, []);
 
   function addTask(task) {
-    setTasks([...tasks, task])
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-    let myElement = document.getElementById("inputTask");
-    myElement.value = '';
+    const newTasks = [...tasks, {
+      status: 'progress',
+      id: v4(),
+      text: task
+    }]
+    setTask(newTasks)
+    setTasks(newTasks)
+  }
+
+  function doneTask(task) {
+    const newData = updateTask(task)
+    setTasks(newData)
+  }
+
+  function deleateTask(id) {
+      const newData = removeTask(id)
+      setTasks(newData)
   }
 
   return (
     <div className="App-header">
-      <DoTask tasks={tasks} addTask={addTask}/>
-      { tasks.map((item, index) => <Task key={index} task={item} index={index}/>)}
+      <DoTask addTask={addTask}/>
+      { tasks.map((item, index) => <Task key={index} task={item} doneTask={doneTask} deleateTask={deleateTask}/>)}
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,26 +1,13 @@
-export function Task({ task, index }) {
+export function Task({ task, doneTask, deleateTask }) {
 
-    function doneTask() {
-        let myElement = document.getElementById(index);
-        myElement.style.textDecoration = "line-through";
-    }
-
-    function deleateTask(index) {
-        const myArray = JSON.parse(localStorage.getItem('tasks'));
-        console.log(myArray);
-        myArray.splice(index,1);
-        console.log(myArray);
-        localStorage.setItem('tasks', JSON.stringify(myArray));
-        let myElement = document.getElementById(index);
-        myElement.remove();
-    }
+    
 
     return (
-        <div className="taskContainer" id={index}>
-            <p className="taskText">{task}</p>
+        <div className={`taskContainer taskStatus_${task.status}`}>
+            <p className="taskText">{task.text}</p>
             <div className="actionButtons">
-                <button className="done" onClick={() => doneTask()}>Сделано</button>
-                <button className="delete" onClick={() => deleateTask(index)}>Удалить</button>
+                {task.status === 'progress' && <button className="done" onClick={() => doneTask({...task, status: 'done'})}>Сделано</button>}
+                <button className="delete" onClick={() => deleateTask(task.id)}>Удалить</button>
             </div>
         </div>
     )
